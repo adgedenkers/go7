a91252ad6ed2568e712279a040e6d1027f062ec6
Adge Denkers <adge.denkers@gmail.com>
2011-08-22 13:48:08 -0400

    added initial db values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Bouncer.create(:title => "VA Acronym Database", :url => "http://vaww.va.gov/acronyms/", :count => 2154, :chars => "vaacronyms", :user_id => 1)
+Snippet.create(:content => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", :user_id => 1, :title => "Lorem Ipsum", :count => 1004)
